7f830a61de6dcc3732c404a808c45b455b862dbd
Jeff Tian <jeff.tian@outlook.com>
2021-02-02 20:33:20 +0800

    feat: learn for linear unit

--- src/LinearUnit.ts
+++ src/LinearUnit.ts
@@ -1,5 +1,32 @@
 import { PerceptronBase } from './index'
 
-export const linearUnit = (...weights: number[]): ((...x: number[]) => number) => {
-  return (...x: number[]) => PerceptronBase.sumProduct(weights, x)
+export const linearUnit = (...weights: number[]): ((...x: number[]) => number) => (...x: number[]) =>
+  PerceptronBase.sumProduct(weights, x)
+
+export const updatedWeights = (weights: number[], o: number, eta, trainingRecord) => {
+  const { x, t } = trainingRecord
+  const delta = (i: number) => eta * (t - o) * x[i]
+
+  return weights.map((w, i) => w + delta(i))
+}
+
+export const learn = (initialWeights: number[], initialEta, trainingSet) => {
+  const learningDetails: any[] = []
+
+  let weights = initialWeights
+  trainingSet.forEach((trainingRecord, i) => {
+    const o = linearUnit(...weights)(...trainingRecord.x)
+    const t = trainingRecord.t
+
+    learningDetails.push({
+      iter: i,
+      weights,
+      o,
+      t,
+      e: t - o,
+    })
+    weights = updatedWeights(weights, o, initialEta / (i + 1), trainingRecord)
+  })
+
+  return learningDetails
 }
--- src/test/LinearUnit.test.ts
+++ src/test/LinearUnit.test.ts
@@ -1,8 +1,43 @@
 import assert = require('assert')
-import { linearUnit } from '../LinearUnit'
+import { learn, linearUnit } from '../LinearUnit'
 
 describe('Linear Unit', () => {
   it('[1] * [1] => 1', () => {
     assert(linearUnit(1)(1) === 1)
+  })
+
+  it('learns', () => {
+    const initialWeights = [1, 1]
+    const initialEta = 0.1
+    const trainingSet = [
+      {
+        x: [1, 1],
+        t: 1,
+      },
+      {
+        x: [2, 2],
+        t: 4,
+      },
+    ]
+
+    const details = learn(initialWeights, initialEta, trainingSet)
+
+    assert(details.length === 2)
+    assert.deepStrictEqual(details, [
+      {
+        e: -1,
+        iter: 0,
+        o: 2,
+        t: 1,
+        weights: [1, 1],
+      },
+      {
+        e: 0.3999999999999999,
+        iter: 1,
+        o: 3.6,
+        t: 4,
+        weights: [0.9, 0.9],
+      },
+    ])
   })
 })
