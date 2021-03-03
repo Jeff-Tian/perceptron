7129ef7fd27a44a25f179442471076e7d8dc2c8d
Jeff Tian <jeff.tian@outlook.com>
2021-03-03 20:04:32 +0800

    fix: generate the gradient descent training

--- src/GradientDescent.ts
+++ src/GradientDescent.ts
@@ -1,29 +1,39 @@
 import { linearUnit } from "./LinearUnit"
 
-export const trainLinearUnit = (trainingExamples, eta) => {
-    let weights = new Array(trainingExamples[0].x.length).fill(0).map(() => Math.random())
-    let deltaWeights = new Array(weights.length).fill(0)
+export const trainLinearUnit = (trainingExamples, eta: number) => {
+    return train(trainingExamples, eta)(computeLinearUnitDeltaWeight)
+}
 
-    const details: any[] = []
+export function train(trainingExamples: any, eta: number) {
+    return (computeDeltaWeight) => {
+        let weights = new Array(trainingExamples[0].x.length).fill(0).map(() => Math.random())
+        let deltaWeights = new Array(weights.length).fill(0)
 
-    trainingExamples.forEach((example, iter) => {
-        const o = linearUnit(...weights)(...example.x)
+        const details: any[] = []
 
-        weights.forEach((_w, i) => {
-            deltaWeights[i] = deltaWeights[i] + eta * (example.t - o) * example.x[i]
+        trainingExamples.forEach((example, iter) => {
+            const o = linearUnit(...weights)(...example.x)
 
-            weights[i] = weights[i] + deltaWeights[i]
+            weights.forEach((_w, i) => {
+                deltaWeights[i] = computeDeltaWeight(deltaWeights[i], eta, example.t, example.x[i], o)
+
+                weights[i] = weights[i] + deltaWeights[i]
+            })
+
+            details.push({
+                iter,
+                weights: [...weights],
+                o,
+                t: example.t,
+                e: example.t - o,
+            })
+
         })
 
-        details.push({
-            iter,
-            weights: [...weights],
-            o,
-            t: example.t,
-            e: example.t - o,
-        })
+        return details
+    }
+}
 
-    })
-
-    return details
+function computeLinearUnitDeltaWeight(deltaWeight: number, eta: number, t: number, x: number, o: number): any {
+    return deltaWeight + eta * (t - o) * x
 }
--- src/test/GradientDescent.test.ts
+++ src/test/GradientDescent.test.ts
@@ -1,10 +1,20 @@
 import assert from "assert"
 import { trainingSet } from "./common/training"
-import { trainLinearUnit } from "../GradientDescent"
+import { train, trainLinearUnit } from "../GradientDescent"
 
 describe("GradientDescent", () => {
     it("trains linear unit", () => {
         const details = trainLinearUnit(trainingSet, 0.05)
+        assert(details.length === 5)
+    })
+
+    it("trains o = w0 + w1 x1 + w1 x1^2 + ... + w2 x2 + w2 x2^2", () => {
+        const trainingExamples = trainingSet.map(t => ({
+            x: [1, t.x[0] + t.x[0] * t.x[0], t.x[1] + t.x[1] * t.x[1]],
+            t: t.t
+        }))
+
+        const details = train(trainingExamples, 0.05)((deltaWeight: number, eta: number, t: number, x: number, o: number) => deltaWeight + eta * (t - o) * x * (1 + x))
         assert(details.length === 5)
     })
 })
