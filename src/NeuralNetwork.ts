47173702299139b3a55b57956dc5e7c0f53abd0c
Jeff Tian <jeff.tian@outlook.com>
2021-04-13 15:48:55 +0800

    feat: learns with alpha enabled ann

--- src/NeuralNetwork.ts
+++ src/NeuralNetwork.ts
@@ -1,10 +1,10 @@
 import { sigmoidThresholdUnit } from './SigmoidThresholdUnit'
 import { PerceptronBase } from './index'
 
-const learnOneStep = (network: INetwork, eta: number) => (input, index) => {
+const learnOneStep = (network: INetwork, eta: number, alpha: number = 0) => (input, index) => {
   forwardProp(input, network)
   backwardProp(input.t, network)
-  updateWeights(input.x, eta, network)
+  updateWeights(input.x, eta, network, alpha)
 
   network.weightsHistory.push({ index, weights: snapshotWeights(network.layers) })
 }
@@ -21,19 +21,21 @@ export const backPropFor2LevelSigmoidUnitForwardNetwork = (
     },
   ],
   eta: number = 0.3,
+  alpha: number = 0,
   numberOfInput: number = 2,
   numberOfOutput: number = 1,
   numberOfHiddenLayers: number = 1,
 ) => {
   const network: INetwork = {
     layers: [
-      { units: [{ weights: [0.1, 0.1, 0.1], output: 0, delta: 0 }] },
+      { units: [{ weights: [0.1, 0.1, 0.1], output: 0, delta: 0, deltas: [0, 0] }] },
       {
         units: [
           {
             weights: [0.1, 0.1],
             output: 0,
             delta: 0,
+            deltas: [0, 0],
           },
         ],
       },
@@ -45,7 +47,7 @@ export const backPropFor2LevelSigmoidUnitForwardNetwork = (
 
   let epoch = 0
   while (epoch++ < 1) {
-    trainingExamples.forEach(learnOneStep(network, eta))
+    trainingExamples.forEach(learnOneStep(network, eta, alpha))
   }
 
   return network
@@ -53,18 +55,18 @@ export const backPropFor2LevelSigmoidUnitForwardNetwork = (
 
 const snapshotWeights = (layers: ILayer[]): number[][][] => layers.map(layer => layer.units.map(u => [...u.weights]))
 
-const updateWeights = (x: number[], eta: number, network: INetwork) => {
+const updateWeights = (x: number[], eta: number, network: INetwork, alpha: number = 0) => {
   network.layers[0].units.forEach(u => {
     x.forEach((xji, index) => {
-      const dwji = eta * u.delta * xji
-      u.weights[index] = u.weights[index] + dwji
+      u.deltas[index] = eta * u.delta * xji + alpha * u.deltas[index]
+      u.weights[index] = u.weights[index] + u.deltas[index]
     })
   })
 
   network.layers[1].units.forEach(u => {
     network.layers[0].units.forEach((xji, index) => {
-      const dwji = eta * u.delta * xji.output
-      u.weights[index] = u.weights[index] + dwji
+      u.deltas[index] = eta * u.delta * xji.output + alpha * u.deltas[index]
+      u.weights[index] = u.weights[index] + u.deltas[index]
     })
   })
 }
@@ -111,6 +113,7 @@ export interface ILayer {
 
 export interface IUnit {
   delta: number
+  deltas: number[]
   output: number
   weights: number[]
 }
--- src/test/NeuralNetwork.test.ts
+++ src/test/NeuralNetwork.test.ts
@@ -10,6 +10,7 @@ describe('Neural Network', () => {
         units: [
           {
             delta: -0.0033547366595323056,
+            deltas: [0, -0.0010064209978596916],
             output: 0.549833997312478,
             weights: [0.10085128181864877, 0.09899357900214031, 0.1],
           },
@@ -19,6 +20,7 @@ describe('Neural Network', () => {
         units: [
           {
             delta: -0.13439178714582642,
+            deltas: [-0.02216795205970723, 0],
             output: 0.5412512661717976,
             weights: [0.09674244573947247, 0.1],
           },
@@ -37,6 +39,47 @@ describe('Neural Network', () => {
       {
         index: 1,
         weights: [[[0.10085128181864877, 0.09899357900214031, 0.1]], [[0.09674244573947247, 0.1]]],
+      },
+    ])
+  })
+
+  it('learns with alpha', () => {
+    const network = backPropFor2LevelSigmoidUnitForwardNetwork(undefined, undefined, 0.9)
+
+    assert.deepStrictEqual(network.layers, [
+      {
+        units: [
+          {
+            delta: -0.0033547366595323056,
+            deltas: [0.0007661536367838874, -0.0010064209978596916],
+            output: 0.549833997312478,
+            weights: [0.10161743545543266, 0.09899357900214031, 0.1],
+          },
+        ],
+      },
+      {
+        units: [
+          {
+            delta: -0.13439178714582642,
+            deltas: [-0.0051485940404455005, 0],
+            output: 0.5412512661717976,
+            weights: [0.1137618037587342, 0.1],
+          },
+        ],
+      },
+    ])
+    assert.deepStrictEqual(network.weightsHistory, [
+      {
+        index: -1,
+        weights: [[[0.1, 0.1, 0.1]], [[0.1, 0.1]]],
+      },
+      {
+        index: 0,
+        weights: [[[0.10085128181864877, 0.1, 0.1]], [[0.1189103977991797, 0.1]]],
+      },
+      {
+        index: 1,
+        weights: [[[0.10161743545543266, 0.09899357900214031, 0.1]], [[0.1137618037587342, 0.1]]],
       },
     ])
   })
